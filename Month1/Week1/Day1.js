f29033af3e0cfdcfe9723e8fb957ca722a47772c
Nikhil Patel <68939870+naive-nikhil@users.noreply.github.com>
2025-06-30 15:54:11 +0530

    Week 1 Day-1

--- Month1/Week1/Day1.js
+++ Month1/Week1/Day1.js
@@ -2,3 +2,15 @@
 // Create and array with 5 Students names, after that create a function which takes 2 parameters (allStudents, studentName) and iterate over all students and find that specific user "studentName".
 
 // Solution
+const allStudents = ["Nikhil", "Suyash", "Prashant", "Nishi", "Varsha"];
+const studentName = "Varsha";
+
+function findStudent(allStudents, studentName) {
+  for (let i = 0; i < allStudents.length; i++) {
+    if (allStudents[i] === studentName) {
+      console.log("Found", studentName);
+    }
+  }
+}
+
+findStudent(allStudents, studentName);
