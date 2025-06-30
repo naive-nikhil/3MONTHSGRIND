eced355c6d435d9c26d371405c96b07ab802384c
Nikhil Patel <68939870+naive-nikhil@users.noreply.github.com>
2025-06-30 15:38:37 +0530

    First Commit

---
+++ Month1/Week1/Day1.js
@@ -0,0 +1,4 @@
+// Question 1
+// Create and array with 5 Students names, after that create a function which takes 2 parameters (allStudents, studentName) and iterate over all students and find that specific user "studentName".
+
+// Solution
