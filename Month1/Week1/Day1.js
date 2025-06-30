8949551f911a11d0b18c685de5aa9e9041f8d60f
Nikhil Patel <68939870+naive-nikhil@users.noreply.github.com>
2025-06-30 16:17:44 +0530

    30-06-2025 Day one Complete

--- Month1/Week1/Day1.js
+++ Month1/Week1/Day1.js
@@ -1,10 +1,16 @@
+// Date - 30th June 2025
+// LeetCode Question for Today - Two Sum, Best time to Buy and Sell Stock, Contain Duplicates
+
 // Question 1
 // Create and array with 5 Students names, after that create a function which takes 2 parameters (allStudents, studentName) and iterate over all students and find that specific user "studentName".
 
 // Solution
+
+//Data Structure 👇
 const allStudents = ["Nikhil", "Suyash", "Prashant", "Nishi", "Varsha"];
 const studentName = "Varsha";
 
+// Algorithm used for finding a specific user (Linear Search) 👇
 function findStudent(allStudents, studentName) {
   for (let i = 0; i < allStudents.length; i++) {
     if (allStudents[i] === studentName) {
